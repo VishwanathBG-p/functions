4c72c6c64ecefaa699a8b04a19394e9387278bea
vishwanathB <vishwanathbg90@gmail.com>
2021-02-04 13:12:43 +0530

    added

---
+++ function1.js
@@ -0,0 +1,33 @@
+//parametr passing
+/*
+function squre(number){
+    return number*number;
+
+}
+console.log(squre(2));
+
+*/
+//object refrences
+/*
+function myfun(obj){
+    obj.make="hero";
+}
+
+let car={make:"honda",year:2000,value:2525}
+let x,y
+x=car.make;
+console.log(x)
+myfun(car)
+y=car.make;
+console.log(y);*/
+
+//factorial function
+/*
+const squre=function(number){return number*number}
+const a=squre(5);
+console.log(a);
+
+const fact=function fac(n){return n<2?1:n* fac(n-1)}
+console.log(fact(1))
+*/
+
